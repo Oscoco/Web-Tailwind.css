497e9218b08b34246bfeaa5d3ba026971fc4e32c
Oscar <oscargonz150@gmail.com>
2022-04-19 00:07:09 -0600

    card animations

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+// mobile menu
+const menuButton = document.querySelector('#mobile-menu');
+
+        menuButton.addEventListener('click', e => {
+            const menu = document.querySelector('.mobile-links');
+
+            menu.classList.toggle('hidden');
+        });
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,22 @@ module.exports = {
     ],
     darkMode: false, //or 'media' or 'class' 
     theme: {
-        extend: {
+         extend: { //aqui configuramos las imagenes las mandamos a llamar
             backgroundImage: theme => ({
                 'wave-pattern': 'url(../img/wave-white.png)',
-                'wave-pattern-dots': 'url(../img/pattern-white-dots.png)'
-            })
+                'wave-pattern-dots': 'url(../img/pattern-white-dots.png)',
+                'pattern-characters-red':'url(../img/pattern-characters-red.png)',
+                'character-mario':'url(../img/character-l.png)',
+                'wave-pink':'url(../img/wave-pink.png)',
+                'yellow-dots':'url(../img/pattern-yellow-dots.png)',
+                '':'',
+                '':'',
+                '':'',
+                '':'',
+            }),
+            screens:{
+                'xsm':'320px'
+            }
         }
     },
     variants:{
